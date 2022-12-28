c44804b8045c3bc8348c87b93eed2651d319955a
Vikki <vyxnne@gmail.com>
2022-12-28 16:34:45 +1100

    removed files

--- _site/scripts/project.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var $projectbody = $('.projectbody').imagesLoaded( 
-        function(){
-            // init Masonry after all images have loaded
-            $projectbody.masonry({
-                itemSelector: '.post', 
-            });
-      });
-});
