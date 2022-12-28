bd6c941a08bdcb54d5782064e7de40787e93f0ee
Vikki <vyxnne@gmail.com>
2022-12-28 16:30:38 +1100

    adding sitemap

---
+++ _site/scripts/project.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var $projectbody = $('.projectbody').imagesLoaded( 
+        function(){
+            // init Masonry after all images have loaded
+            $projectbody.masonry({
+                itemSelector: '.post', 
+            });
+      });
+});
